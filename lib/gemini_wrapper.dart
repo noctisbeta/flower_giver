f08431849b325a439b6feadfd75cdaae1e22a10c
noctisbeta <noctisbeta@gmail.com>
2025-02-24 10:33:11 +0100

    revert

--- lib/gemini_wrapper.dart
+++ lib/gemini_wrapper.dart
@@ -1,4 +1,6 @@
+import 'package:flower_giver/ipv6_client.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:http/io_client.dart';
 import 'package:meta/meta.dart';
 
 @immutable
@@ -7,9 +9,9 @@ final class GeminiWrapper {
     : model = GenerativeModel(
         model: 'gemini-2.0-flash',
         apiKey: apiKey,
-        // httpClient: IOClient(
-        //   IPv6Client(targetHost: 'generativelanguage.googleapis.com'),
-        // ),
+        httpClient: IOClient(
+          IPv6Client(targetHost: 'generativelanguage.googleapis.com'),
+        ),
       );
 
   final GenerativeModel model;
